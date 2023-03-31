53872ece057d917fe99dd71010582a587c2d26de
aenewsss <aenamartinelli@gmail.com>
2023-03-31 12:32:46 -0300

    v2 check

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "onixia-backend.vercel.app/",
+    baseURL: "https://onixia-backend.vercel.app/",
     headers: {
         "Content-Type": "application/json",
     }
